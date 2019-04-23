95a50a91b37859a17afb186ede194fbf54dcca93
Jae Bradley <jae.b.bradley@gmail.com>
2019-04-22 21:17:25 -0700

    Initial jump game

---
+++ jump_game.py
@@ -0,0 +1,43 @@
+"""
+https://leetcode.com/problems/jump-game/
+
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+             jump length is 0, which makes it impossible to reach the last index.
+"""
+
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) <= 0:
+            return False
+
+        counter = nums[0]
+
+        for index, num in enumerate(nums):
+            counter -= index + 1
+
+            if counter < 0:
+                return False
+
+            counter += num
+
+        return True
