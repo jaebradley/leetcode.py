6e9cc83e1e3a7727af02c4845f796dd1b4930e53
Jae Bradley <jae.b.bradley@gmail.com>
2019-09-08 22:46:50 -0700

    Serialize and deserialize binary tree

---
+++ test_serialize_and_deserialize_binary_tree.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from tree_node import TreeNode
+from serialize_and_deserialize_binary_tree import Codec
+
+
+class TestCodec(TestCase):
+    def test_encode_empty_left_children(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.right.left = TreeNode(4)
+        root.right.right = TreeNode(5)
+        self.assertEqual(
+            "1 2 3 none none 4 5",
+            Codec().serialize(root)
+        )
+
+    def test_decode_empty_left_children(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.right.left = TreeNode(4)
+        root.right.right = TreeNode(5)
+        self.assertEqual(
+            root,
+            Codec().deserialize("1 2 3 none none 4 5")
+        )
